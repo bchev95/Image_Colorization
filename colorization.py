c0d1580eeb1f03f333ef52572e8ee53e075284a4
bchev95 <brett.chevalier@gmail.com>
2022-04-12 14:29:25 -0400

    implemented image selection

--- colorization.py
+++ colorization.py
@@ -12,37 +12,37 @@ img_path = 'input_imgs/nyc.jpg'
 
 #TO DO: Take in filename from command line, append name to "input_imgs/", then use this as path
 
-input_img = cv2.imread(img_path)
+    input_img = cv2.imread(img_path)
 
-#Check that input image file exists and was read correctly, else exit
-if input_img is None:
-    exit('Error: Input file does not exist, exiting')
+    #Check that input image file exists and was read correctly, else exit
+    if input_img is None:
+        exit('Error: Input file does not exist, exiting')
 
-#pts_in_hull.npy contains 313 cluster kernels computed from stacking a and b values in 2D
-pts_file = np.load(points_path)
+    #pts_in_hull.npy contains 313 cluster kernels computed from stacking a and b values in 2D
+    pts_file = np.load(points_path)
 
-#Load pre-trained caffemodel into net_cf variable
-net_cf = cv2.dnn.readNetFromCaffe(prototxt_path, cf_model_path)
+    #Load pre-trained caffemodel into net_cf variable
+    net_cf = cv2.dnn.readNetFromCaffe(prototxt_path, cf_model_path)
 
-#Define and load CaffeModel blobs - from opencv documentation
-pts_file = pts_file.transpose().reshape(2, 313, 1, 1)
-net_cf.getLayer(net_cf.getLayerId('class8_ab')).blobs = [pts_file.astype(np.float32)]
-net_cf.getLayer(net_cf.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, np.float32)]
+    #Define and load CaffeModel blobs - from opencv documentation
+    pts_file = pts_file.transpose().reshape(2, 313, 1, 1)
+    net_cf.getLayer(net_cf.getLayerId('class8_ab')).blobs = [pts_file.astype(np.float32)]
+    net_cf.getLayer(net_cf.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, np.float32)]
 
-#Normalize values by scaling them down to fit between 0 and 1 (divide by 255)
-normalized_img = (input_img[:,:,[2, 1, 0]] * 1.0 / 255).astype(np.float32)
-#Convert RGB to LAB - LAB is a color space expressed as L - lightness, A - (green/red), B - (blue/yellow)
-lab_converted = cv2.cvtColor(normalized_img, cv2.COLOR_RGB2Lab)
-#Extract L channel (lightness)
-l_chnl = lab_converted[:,:,0] 
+    #Normalize values by scaling them down to fit between 0 and 1 (divide by 255)
+    normalized_img = (input_img[:,:,[2, 1, 0]] * 1.0 / 255).astype(np.float32)
+    #Convert RGB to LAB - LAB is a color space expressed as L - lightness, A - (green/red), B - (blue/yellow)
+    lab_converted = cv2.cvtColor(normalized_img, cv2.COLOR_RGB2Lab)
+    #Extract L channel (lightness)
+    l_chnl = lab_converted[:,:,0] 
 
-#Model is trained to work with dimensions 224 x 224
-model_width, model_height = 224, 224
+    #Model is trained to work with dimensions 224 x 224
+    model_width, model_height = 224, 224
 
 #Resize image to model dimensions (224 x 224)
-l_resized = cv2.resize(l_chnl, (model_width, model_height)) 
-#Subtract mean value (this value can be tweaked)
-l_resized -= 50
+    l_resized = cv2.resize(l_chnl, (model_width, model_height)) 
+    #Subtract mean value (this value can be tweaked)
+    l_resized -= 50
 
 net_cf.setInput(cv2.dnn.blobFromImage(l_resized))
 ab_chnl = net_cf.forward()[0,:,:,:].transpose((1,2,0)) 
